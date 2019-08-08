05a2958eaebbc9c37e206ef14a47760243d87e15
unknown <testuser@shanliupc.fareast.corp.microsoft.com>
2019-08-08 14:12:29 +0800

    sdk rebuild&test

--- Android/CEFSDK/src/main/java/com/microsoft/cef/prodsdk/utils/CEFConstants.java
+++ Android/CEFSDK/src/main/java/com/microsoft/cef/prodsdk/utils/CEFConstants.java
@@ -7,7 +7,7 @@ public class CEFConstants {
 
     //--------------CEFServiceManager-----------------------
     private final static String API_VERSION = "2018-10-01";
-    final static String CEFSERVICE_DEFAULT_ENDPOINT = "https://cef.chinacloudapi.cn";
+    final static String CEFSERVICE_DEFAULT_ENDPOINT = "http://40.73.76.135";
 
     //--------------CEFServiceOTP------------------------
     public final static String CEFSERVICE_OTP_START = "/services/otp/start?api-version="+ API_VERSION;
