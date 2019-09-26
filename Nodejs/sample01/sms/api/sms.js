59ea9365f7add617b4bbdc5f5a0c3c5bbff2033f
Wang Jin3 <wang.jin3@oe.21vianet.com>
2019-09-26 15:00:09 +0800

    nodejs

---
+++ Nodejs/sample01/sms/api/sms.js
@@ -0,0 +1,26 @@
+const { httpClientSAS, } = require('./../../http/client');
+
+
+function sendMessage(accountName, message) {
+    
+    
+    const url = `services/sms/messages`;
+    
+    return httpClientSAS.request({
+        // `url` is the server URL that will be used for the request
+        url: url,
+        // `method` is the request method to be used when making the request
+        method: 'post', // default
+        params: {
+            'api-version': '2018-10-01'
+        },
+        data: message
+    
+    }).then(res => {
+        console.log(res.data);
+        return res.data;
+    }, err => {
+        console.log(err.response.data);
+    });
+    
+}
